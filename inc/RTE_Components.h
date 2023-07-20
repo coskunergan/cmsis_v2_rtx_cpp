54e410a5227af1b16c59abd0078fd9e960c496f7
coskunergan <coskun.ergan@gtmeu.com.tr>
2023-07-20 16:50:43 +0300

    V1.0.0
    -Files uploaded.

---
+++ inc/RTE_Components.h
@@ -0,0 +1,28 @@
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: Blinky
+ * RTE configuration: Blinky.rteconfig
+*/
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/*
+ * Define the Device Header File:
+*/
+#define CMSIS_device_header "stm32l1xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.3 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+// #define RTE_Compiler_EventRecorder
+//           #define RTE_Compiler_EventRecorder_DAP
+/* Keil.ARM Compiler::Compiler:I/O:STDOUT:EVR:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_EVR      /* Compiler I/O: STDOUT EVR */
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+/*
+    Main Program
+
+    Created on: March 13, 2023
+
+    Author: Coskun ERGAN
+*/
+#include "printf_io.h"
+#include "OS.h"
+#include "Thread.h"
+#include "Timer.h"
+#include "Mutex.h"
+#include "Chrono.h"
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
+int main(int argc, char **argv)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    printf_init();
+
+    printf("Restart..\r\n");
+
+    cmsis::kernel::initialize();
+
+    std::thread main_thread([]
+    {
+        sys::timer ledTimer(std::chrono::milliseconds(200), []
+        {
+            GPIO_ToggleBits(GPIOB, GPIO_Pin_0);
+            return true;
+        });
+        ledTimer.start();
+        sys::chrono::high_resolution_clock::time_point tp1 = sys::chrono::high_resolution_clock::now();
+        std::mutex my_mutex;
+        for(;;)
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(900));
+            my_mutex.lock();
+            printf("Run: %d        \r\n", (sys::chrono::high_resolution_clock::now() - tp1).count());
+            my_mutex.unlock();
+        }
+    });
+
+    cmsis::kernel::start();
+
+    return 0;
+}
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
